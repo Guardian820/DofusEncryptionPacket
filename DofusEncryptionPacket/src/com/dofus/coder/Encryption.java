bd21b8be963886196900f9a6f29e3ff6418d762e
Steven W <Manghao@users.noreply.github.com>
2017-09-26 11:12:59 +0200

    Update Encryption classe

--- DofusEncryptionPacket/src/com/dofus/coder/Encryption.java
+++ DofusEncryptionPacket/src/com/dofus/coder/Encryption.java
@@ -9,8 +9,8 @@ package com.dofus.coder;
  * */
 public class Encryption {
 
-	private String[] _aKeys;
-	private int _nCurrentKey;
+	private String[] keys;
+	private int currentKey;
 	private String HEX_CHARS[] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
 	
 	public Encryption(String packet) {
@@ -23,30 +23,30 @@ public class Encryption {
 		if (packet.substring(0, 2).compareTo("AT") == 0) this.onKey(packet.substring(3));
 	}
 	
-	private void onKey(String sExtraData) { 
-		int _loc3 = Integer.parseInt(sExtraData.substring(0, 1), 16);
-		String _loc4 = sExtraData.substring(1);
-		this.addKeyToCollection(_loc3, _loc4);
-		this.startUsingKey(_loc3);
+	private void onKey(String data) { 
+		int i = Integer.parseInt(data.substring(0, 1), 16);
+		String str = data.substring(1);
+		this.addKeyToCollection(i, str);
+		this.startUsingKey(i);
 	}
 	
-	private void addKeyToCollection(int nKeyID, String sKey) {
-		if (this._aKeys == null) {
-			this._aKeys = new String[nKeyID + 1];
+	private void addKeyToCollection(int keyId, String key) {
+		if (this.keys == null) {
+			this.keys = new String[keyId + 1];
 		}
-		this._aKeys[nKeyID] = this.prepareKey(sKey);
+		this.keys[keyId] = this.prepareKey(key);
 	}
 
-	private void startUsingKey(int nKeyID) {
-		this._nCurrentKey = nKeyID;
+	private void startUsingKey(int keyId) {
+		this.currentKey = keyId;
 	}
 	
 	private String prepareKey(String d) {
-		String _loc3 = new String();
-		for (int _loc4 = 0; _loc4 < d.length(); _loc4 = _loc4 + 2) {
-			_loc3 = _loc3 + (char)Integer.parseInt(d.substring(_loc4, (d.length() % 2 == 0) ? _loc4 + 2 : _loc4 + 1), 16);
+		String str = new String();
+		for (int i = 0; i < d.length(); i = i + 2) {
+			str = str + (char)Integer.parseInt(d.substring(i, (d.length() % 2 == 0) ? i + 2 : i + 1), 16);
 		}
-		return _loc3;
+		return str;
 	}
 	
 	private String d2h(int d) {
@@ -55,66 +55,66 @@ public class Encryption {
 	}
 
 	private String preEscape(String s) {
-		String _loc3 = new String();
-		int _loc4 = 0;
-		while (_loc4 < s.length()) {
-			String _loc5 = Character.toString(s.charAt(_loc4));
-			int _loc6 = (int)s.charAt(_loc4);
-			if (_loc6 < 32 || (_loc6 > 127 || (_loc5 == "%" || _loc5 == "+")))
+		String str = new String();
+		int i = 0;
+		while (i < s.length()) {
+			String ope = Character.toString(s.charAt(i));
+			int j = (int)s.charAt(i);
+			if (j < 32 || (j > 127 || (ope == "%" || ope == "+")))
 			{
-				_loc3 = _loc3 + _loc5.replaceAll("%", "").replaceAll("+", "");
+				str = str + ope.replaceAll("%", "").replaceAll("+", "");
 				continue;
 			}
-			_loc3 = _loc3 + _loc5;
-			_loc4++;
+			str = str + ope;
+			i++;
 		}
-		return _loc3;
+		return str;
 	}
 
 	private String checksum(String s) {
-		int _loc3 = 0;
-		for (int _loc4 = 0; _loc4 < s.length(); _loc4++) {
-			_loc3 = _loc3 + (((int) s.charAt(_loc4)) % 16);
+		int j = 0;
+		for (int i = 0; i < s.length(); i++) {
+			j = j + (((int) s.charAt(i)) % 16);
 		}
-		return this.HEX_CHARS[_loc3 % 16];
+		return this.HEX_CHARS[j % 16];
 	}
 	
 	public String prepareData(String s) {
-		if (this._nCurrentKey == 0 || this._nCurrentKey < 0) return s;
-		if (this._aKeys[this._nCurrentKey] == null) return s;
-		String _loc3 = HEX_CHARS[this._nCurrentKey];
-		String _loc4 = checksum(s);
-		_loc3 = _loc3 + _loc4;
-		return _loc3 + cypherData(s, this._aKeys[this._nCurrentKey], Integer.parseInt(_loc4, 16) * 2);
+		if (this.currentKey == 0 || this.currentKey < 0) return s;
+		if (this.keys[this.currentKey] == null) return s;
+		String str = HEX_CHARS[this.currentKey];
+		String checkum = checksum(s);
+		str = str + checkum;
+		return str + this.cypherData(s, this.keys[this.currentKey], Integer.parseInt(checkum, 16) * 2);
 	}
 
 	private String cypherData(String d, String k, int c) {
-		String _loc5 = new String();
-		int _loc6 = k.length();
+		String str = new String();
+		int j = k.length();
 		d = preEscape(d);
-		for (int _loc7 = 0; _loc7 < d.length(); _loc7++) {
-			_loc5 = _loc5 + d2h((int)d.charAt(_loc7) ^ (int)k.charAt((_loc7 + c) % _loc6));
+		for (int i = 0; i < d.length(); i++) {
+			str = str + this.d2h((int)d.charAt(i) ^ (int)k.charAt((i + c) % j));
 		}
-		return _loc5;
+		return str;
 	}
 
 	public String unprepareData(String s) {	
-		if (this._nCurrentKey == 0) return s;
-		String _loc3 = this._aKeys[Integer.parseInt(s.substring(0, 1), 16)];
-		if (_loc3 == null) return s;
-		String _loc4 = s.substring(1, 2).toUpperCase();
-		String _loc5 = decypherData(s.substring(2), _loc3, Integer.parseInt(_loc4, 16) * 2);
-		//if (checksum(_loc5).compareTo(_loc4) != 0) return s;
-		return _loc5;
+		if (this.currentKey == 0) return s;
+		String str = this.keys[Integer.parseInt(s.substring(0, 1), 16)];
+		if (str == null) return s;
+		String sub = s.substring(1, 2).toUpperCase();
+		String decypherDaga = this.decypherData(s.substring(2), str, Integer.parseInt(sub, 16) * 2);
+		//if (checksum(decypherDaga).compareTo(sub) != 0) return s;
+		return decypherDaga;
 	}
 
 	private String decypherData(String d, String k, int c) {		
-		String _loc5 = new String();
-		int _loc6 = k.length();
-		int _loc7 = 0;
-		for (int _loc9 = 0; _loc9 < d.length(); _loc9 = _loc9 + 2) {
-			_loc5 = _loc5 + Character.toString((char) (Integer.parseInt(d.substring(_loc9, _loc9 + 2), 16) ^ (int)k.charAt((_loc7++ + c) % _loc6)));
+		String str = new String();
+		int j = k.length();
+		int l = 0;
+		for (int i = 0; i < d.length(); i = i + 2) {
+			str = str + Character.toString((char) (Integer.parseInt(d.substring(i, i + 2), 16) ^ (int)k.charAt((l++ + c) % j)));
 		}
-		return _loc5;
+		return str;
 	}
 }
